33bac221c6bdf664b5caf7945a993c07aeb140c0
murodjon <toshtemirovmurodjon03@gmail.com>
2022-06-19 17:05:24 +0500

    JavaScript exam

--- taskfour.js
+++ taskfour.js
@@ -1,1 +1,18 @@
-  
+  // -----------------------4-masala-----
+
+// var obj = {}
+
+// function maxPrice(object) {
+//   var obj = [
+//     {name:'Ali',price:200},
+//     {name:'Umid',price:800},
+//     {name:'Sardor',price:400}
+//   ]
+//   if(obj.price){
+//     obj == 5
+//   }
+
+//   return obj;
+// }
+
+// console.log(maxPrice());
